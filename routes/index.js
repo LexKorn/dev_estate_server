4544d07d94e2de11bb182c44b95e7550201f1227
LexKorn <fischer84@mail.ru>
2024-02-27 07:23:52 +0300

    edited index routes

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = new Router();
 const flatsRouter = require('./flatsRouter');
 const likesRouter = require('./likesRouter');
 const comparesRouter = require('./comparesRouter');
+const usersRouter = require('./usersRouter');
 
 router.use('/flats', flatsRouter);
 router.use('/likes', likesRouter);
 router.use('/compares', comparesRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
